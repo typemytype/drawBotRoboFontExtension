d35707a3ffa1c52621f3c8f5c10d5d3961f1a396
Frederik Berlaen <frederik@typemytype.com>
2014-12-04 19:22:32 +0300

    optimize the install and support to open .py files directly in drawBot

--- DrawBot.roboFontExt/lib/installDrawbot.py
+++ DrawBot.roboFontExt/lib/installDrawbot.py
@@ -1,4 +1,56 @@
 import sys
 import os
 
+from fontTools.pens.cocoaPen import CocoaPen
+
+from drawBot.drawBotDrawingTools import _drawBotDrawingTool
+from drawBot.context.baseContext import BezierPath
+
+from mojo.events import addObserver
+from mojo.extensions import getExtensionDefault
+
+from drawBotController import DrawBotController
+
 sys.path.append(os.path.dirname(__file__))
+
+def drawGlyph(glyph):
+    if hasattr(glyph, "getRepresentation"):
+        path = glyph.getRepresentation("defconAppKit.NSBezierPath")
+    else:
+        pen = CocoaPen(glyph.getParent())
+        glyph.draw(pen)
+        path = pen.path
+    _drawBotDrawingTool.drawPath(path)
+
+_drawBotDrawingTool.drawGlyph = drawGlyph
+
+class RFBezierPath(BezierPath):
+
+    def addGlyph(self, glyph):
+        if hasattr(glyph, "getRepresentation"):
+            path = glyph.getRepresentation("defconAppKit.NSBezierPath")
+        else:
+            pen = CocoaPen(glyph.getParent())
+            glyph.draw(pen)
+            path = pen.path
+        self.getNSBezierPath().appendBezierPath_(path)
+
+_drawBotDrawingTool._bezierPathClass = RFBezierPath
+
+# reload the module to make them everwhere available
+import drawBot
+reload(drawBot)
+
+class OpenFilesInDrawBotController(object):
+
+    def __init__(self):
+        addObserver(self, "openFile", "applicationOpenFile")
+
+    def openFile(self, notification):
+        if getExtensionDefault("com.drawBot.openPyFileDirectly", False):
+            fileHandler = notification["fileHandler"]
+            path = notification["path"]
+            DrawBotController().open(path)
+            fileHandler["opened"] = True
+
+OpenFilesInDrawBotController()
