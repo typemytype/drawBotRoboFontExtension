5e77ed6c4027634ed9aa27f682c50adb36fab060
Frederik Berlaen <frederik@typemytype.com>
2014-12-04 19:22:15 +0300

    adding settings

---
+++ DrawBot.roboFontExt/lib/settings.py
@@ -0,0 +1,19 @@
+from vanilla import *
+
+from mojo.extensions import getExtensionDefault, setExtensionDefault
+
+class DrawBotSettingsController(object):
+
+    def __init__(self):
+        self.w = Window((250, 45), "DrawBot Settings")
+
+        self.w.openPythonFilesInDrawBot = CheckBox((10, 10, -10, 22), "Open .py files directly in DrawBot.", 
+                                                value=getExtensionDefault("com.drawBot.openPyFileDirectly", False),
+                                                callback=self.openPythonFilesInDrawBotCallback)
+
+        self.w.open()
+
+    def openPythonFilesInDrawBotCallback(self, sender):
+        setExtensionDefault("com.drawBot.openPyFileDirectly", sender.get())
+
+DrawBotSettingsController()
