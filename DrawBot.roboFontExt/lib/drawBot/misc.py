60ce7e1095fc8f30369f31944a32fe06b0d384c6
Frederik Berlaen <frederik@typemytype.com>
2016-01-11 10:33:48 +0100

    update from master

--- DrawBot.roboFontExt/lib/drawBot/misc.py
+++ DrawBot.roboFontExt/lib/drawBot/misc.py
@@ -74,11 +74,15 @@ def optimizePath(path):
     return path
 
 
-def formatNumber(value, precision=1000):
+# ================
+# = number tools =
+# ================
+
+def formatNumber(value, decimals=3):
     value = float(value)
     if value.is_integer():
         return "%i" % value
-    value = round(value*precision) / precision
+    value = round(value, decimals)
     return "%s" % value
 
 
@@ -111,10 +115,16 @@ def rgb2cmyk(r, g, b):
     return c, m, y, k
 
 
+# =============
+
 def stringToInt(code):
     import struct
     return struct.unpack('>l', code)[0]
 
+
+# ============
+# = warnings =
+# ============
 
 class Warnings(object):
 
@@ -193,6 +203,7 @@ class VariableController(object):
         self.w.resize(250, y)
 
     def changed(self, sender):
+        self.documentWindowToFront()
         if self._callback:
             self._callback()
 
@@ -205,3 +216,8 @@ class VariableController(object):
 
     def show(self):
         self.w.show()
+
+    def documentWindowToFront(self, sender=None):
+        document = self.w.getNSWindow().document()
+        if document:
+            document.showWindows()
